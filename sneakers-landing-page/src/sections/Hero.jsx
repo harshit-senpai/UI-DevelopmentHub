249c261351522e954d813b2ba5ba4f31e917e269
harshit Mishra <miananya2@gmail.com>
2024-03-19 19:13:25 +0530

    first:Hero

--- sneakers-landing-page/src/sections/Hero.jsx
+++ sneakers-landing-page/src/sections/Hero.jsx
@@ -1,5 +1,64 @@
+import Button from "../components/Button";
+import { arrowRight } from "./../assets/icons";
+import { bigShoe1 } from "../assets/images";
+import { shoes, statistics } from "./../constants/index";
+import ShoeCard from "../components/ShoeCard";
+
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
+    >
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28 ">
+        <p className="text-xl font-montserrat text-coral-red">
+          Our Summer collection
+        </p>
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+            The New Arrival
+          </span>
+          <br />
+          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
+        </h1>
+        <p className="text-slate-gray font-montserrat text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life
+        </p>
+        <Button label="Shop Now" iconURL={arrowRight} />
+        <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16 ">
+          {statistics.map((stat) => (
+            <div key={stat.label}>
+              <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
+              <p className="text-slate-gray leading-7 font-montserrat">
+                {stat.label}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="relative flex flex-1 justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+        <img
+          src={bigShoe1}
+          alt="bigShoe1"
+          height={500}
+          width={610}
+          className="object-contain relative z-10"
+        />
+        <div>
+          {shoes.map((shoe) => (
+            <div key={shoe}>
+              <ShoeCard
+                imgURL={shoe}
+                changeBigShoeImage={() => {}}
+                bigShoeImg=""
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
