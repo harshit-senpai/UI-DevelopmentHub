42ee18c27b4191986e7fcb614e81e764ef583f6a
harshit Mishra <miananya2@gmail.com>
2024-03-19 17:24:10 +0530

    first:Navbar

--- sneakers-landing-page/src/components/Navbar.jsx
+++ sneakers-landing-page/src/components/Navbar.jsx
@@ -1,5 +1,37 @@
+import { headerLogo } from "./../assets/images";
+import { hamburger } from "./../assets/icons";
+import { navLinks } from "../constants";
+
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerLogo} alt="logo" width={130} height={29} />
+        </a>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((item) => (
+            <a
+              href={item.href}
+              key={item.label}
+              className="font-montserrat leading-normal text-lg text-slate-gray"
+            >
+              {item.label}
+            </a>
+          ))}
+        </ul>
+        <div>
+          <img
+            src={hamburger}
+            alt="hamburger"
+            width={25}
+            height={25}
+            className="hidden max-lg:block"
+          />
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Navbar;
