b07d63ae58efcf9a7fe5f85a636bf96b6d8a90fa
harshit Mishra <miananya2@gmail.com>
2024-03-19 16:46:10 +0530

    first:setup

--- sneakers-landing-page/src/App.jsx
+++ sneakers-landing-page/src/App.jsx
@@ -1,9 +1,43 @@
-function App() {
+import Navbar from './components/Navbar'
+import CustomerReview from "./sections/CustomerReview";
+import Footer from "./sections/Footer";
+import Hero from "./sections/Hero";
+import PopularProducts from "./sections/PopularProducts";
+import Services from "./sections/Services";
+import SpecialOffer from "./sections/SpecialOffer";
+import Subscribe from "./sections/Subscribe";
+import SuperQuality from "./sections/SuperQuality";
+
+const App = () => {
   return (
-    <div>
-      <h1 className="text-red-500">Sneakers showcase</h1>
-    </div>
+    <main className="relative">
+      <Navbar />
+      <section className="xl:padding-l wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding ">
+        <PopularProducts />
+      </section>
+      <section className="padding ">
+        <SuperQuality />
+      </section>
+      <section className="padding-x ">
+        <Services />
+      </section>
+      <section className="padding ">
+        <SpecialOffer />
+      </section>
+      <section className="padding bg-pale-blue">
+        <CustomerReview />
+      </section>
+      <section className="padding sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className="padding padding-x padding-t pb-8 bg-black">
+        <Footer />
+      </section>
+    </main>
   );
-}
+};
 
 export default App;
