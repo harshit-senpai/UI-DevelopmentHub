9ccd81cda2c1824a27a7fc734f255dfe6f47dd45
harshit Mishra <miananya2@gmail.com>
2024-03-24 20:41:05 +0530

    quality section

--- sneakers-landing-page/src/sections/SuperQuality.jsx
+++ sneakers-landing-page/src/sections/SuperQuality.jsx
@@ -1,5 +1,31 @@
+import { shoe8 } from "../assets/images";
+import Button from "../components/Button";
+
 function SuperQuality() {
-  return <div>SuperQuality</div>;
+  return (
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 max-container"
+    >
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          We Provide You <span className="text-coral-red">Super</span>{" "}
+          <span className="text-coral-red">Quality</span> shoes
+        </h2>
+        <p className="lg:max-w-lg info-text mt-4">
+          Ensuring preimum comfort and style, our meticulously created footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and touch of elegance
+        </p>
+        <div className="mt-11">
+          <Button label="view details" />
+        </div>
+      </div>
+      <div className="flex-1 flex justify-center items-center">
+        <img src={shoe8} alt="shoe8" width={570} height={522} className="object-contain" />
+      </div>
+    </section>
+  );
 }
 
 export default SuperQuality;
